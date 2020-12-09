76d47be2898d9200db14e0b3e8983b5bd8743a59
vmurali100 <vmurali100@users.noreply.github.com>
2020-12-09 12:34:47 +0530

    some

--- project1/src/Member.jsx
+++ project1/src/Member.jsx
@@ -11,11 +11,19 @@ export const Member = () => {
       <div class="mb-3">
         <input
           type="email"
-          class="form-control"
+          class="form-control memberInput"
           placeholder="Your Member Number"
         />
       </div>
       <p>Forgot your Member Number ? Call CUA on 1800 931 411 to Retrieve it</p>
+      <div className="actionArea text-end">
+        <button type="button" class="btn btn-default m-2 cancel">
+          Cancel
+        </button>
+        <button type="button" class="btn btn-warning m-2 continue">
+          Continue
+        </button>
+      </div>
     </div>
   );
 };
